cb2404ad0f6daa5a4e0efaae9cb1d7e40cc723a6
Laksh <79443832+heyLaksh@users.noreply.github.com>
2022-02-08 15:45:04 +0530

    Done 3rd
    Completed the toggle and mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
 const btnhamburger = document.querySelector('#btnhamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnhamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
     }
     else{
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
     }
 });
